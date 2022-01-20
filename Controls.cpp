b05b974da6789bbd09699ca9130c4d20a8890a58
bradsharp <bradleysharpie@gmail.com>
2022-01-20 10:37:21 +0000

    Use SKSE input addresses

--- Controls.cpp
+++ Controls.cpp
@@ -1,5 +1,6 @@
 #include "Controls.h"
 #include "PatternScanner.h"
+#include "skse64/GameInput.h"
 
 namespace Tralala
 {
@@ -9,14 +10,9 @@ namespace Tralala
 
 	void ControlsGetAddresses()
 	{
-		const std::array<BYTE, 8> playerpattern = { 0x8B, 0x40, 0x10, 0xC1, 0xE8, 0x17, 0xA8, 0x01 };
-		g_playerControlsAddr = (uintptr_t)scan_memory_data(playerpattern, 0x6E, true, 0x3, 0x7);
-
-		const std::array<BYTE, 7> menupattern = { 0xC1, 0xE8, 0x04, 0xA8, 0x01, 0x74, 0x35 };
-		g_menuControlsAddr = (uintptr_t)scan_memory_data(menupattern, 0x27, false, 0x3, 0x7);
-
-		const std::array<BYTE, 8> inputpattern = { 0x48, 0x8B, 0x43, 0x60, 0xF6, 0x40, 0x12, 0x01 };
-		g_inputManagerAddr = (uintptr_t)scan_memory_data(inputpattern, 0x33, false, 0x3, 0x7);
+		g_playerControlsAddr = (uintptr_t)PlayerControls::GetSingleton();
+		g_menuControlsAddr = (uintptr_t)MenuControls::GetSingleton();
+		g_inputManagerAddr = (uintptr_t)InputManager::GetSingleton();
 	}
 
 	MenuControls * MenuControls::GetSingleton()
